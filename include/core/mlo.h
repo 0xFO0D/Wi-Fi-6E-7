4aa800a62c17db927fe87bedd0e475a6f91ce092
REXIN <184462342+rexinscfu@users.noreply.github.com>
2025-02-13 22:15:20 +0100

    mlo: Add link state management

--- include/core/mlo.h
+++ include/core/mlo.h
@@ -8,6 +8,9 @@ enum wifi67_mlo_link_state {
     WIFI67_MLO_LINK_ERROR
 };
 
+#define WIFI67_MLO_LINK_FLAG_ACTIVE  BIT(0)
+#define WIFI67_MLO_LINK_FLAG_ERROR   BIT(1)
+
 struct wifi67_mlo_link {
     struct wifi67_priv *priv;
     struct list_head list;
@@ -23,5 +26,11 @@ int wifi67_mlo_setup_link(struct wifi67_priv *priv, struct wifi67_mlo_link *link
 void wifi67_mlo_remove_link(struct wifi67_mlo_link *link);
 int wifi67_mlo_init(struct wifi67_priv *priv);
 void wifi67_mlo_deinit(struct wifi67_priv *priv);
+
+int wifi67_mlo_activate_link(struct wifi67_mlo_link *link);
+int wifi67_mlo_deactivate_link(struct wifi67_mlo_link *link);
+int wifi67_mlo_handle_link_error(struct wifi67_mlo_link *link);
+struct wifi67_mlo_link *wifi67_mlo_get_link_by_id(struct wifi67_priv *priv,
+                                                 u8 link_id);
 
 #endif /* __WIFI67_MLO_H */ 
